d8fd31ef5dfa0e8d727038cdd992286227117a93
Issac <issacthomasvarghese@gmail.com>
2024-12-04 12:16:10 +0530

    Added IAM Simulate Policy for AWS Permission Check

---
+++ pkg/aws/iam/main.go
@@ -0,0 +1,45 @@
+package iam
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+func HasPermissions() error {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		return fmt.Errorf("unable to load SDK config, %v", err)
+	}
+
+	stc := sts.NewFromConfig(cfg)
+	out, err := stc.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{})
+
+	if err != nil {
+		return fmt.Errorf("error calling sts: %s", err.Error())
+	}
+
+	svc := iam.NewFromConfig(cfg)
+	actions := []string{"mediaconvert:CreateJob", "s3:CopyObject", "s3:DeleteObject", "s3:HeadObject"}
+
+	result, err := svc.SimulatePrincipalPolicy(context.TODO(), &iam.SimulatePrincipalPolicyInput{
+		PolicySourceArn: out.Arn,
+		ActionNames:     actions,
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to simulate policy: %v", err)
+	}
+
+	for _, evaluation := range result.EvaluationResults {
+		if evaluation.EvalDecision != "allowed" {
+			return fmt.Errorf("permission denied for action: %s", *evaluation.EvalActionName)
+		}
+	}
+
+	fmt.Printf("Provided AWS Credentials with arn %s have the required permissions.\n", *out.Arn)
+	return nil
+}
