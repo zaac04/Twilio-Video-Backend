2729753e8a95d3e7465041302fbe3ce328850727
Issac <issacthomasvarghese@gmail.com>
2025-01-23 15:47:51 +0530

    update: secret_arn_reference

--- deploy/beta/update_task_def.sh
+++ deploy/beta/update_task_def.sh
@@ -81,7 +81,7 @@ convert_secret_file_to_json_array() {
         fi
         key=$(echo "$key" | xargs)
         value=$(echo "$value" | xargs)
-        json_array+="{\"name\": \"$key\", \"valueFrom\": \"$secret_arn:$value\"},"
+        json_array+="{\"name\": \"$key\", \"valueFrom\": \"$secret_arn:$value::\"},"
     done <"$filename"
 
     json_array="${json_array%,}]"
