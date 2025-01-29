d264ed91df4986825e1e9867f37595eec4db723f
Issac <issacthomasvarghese@gmail.com>
2025-01-29 14:33:40 +0530

    update:  removed iam simulate policy

--- internal/initilizers/init.go
+++ internal/initilizers/init.go
@@ -5,7 +5,6 @@ import (
 	"stargazer/video-recording/internal/comms"
 	"stargazer/video-recording/internal/db"
 	"stargazer/video-recording/internal/utils"
-	"stargazer/video-recording/pkg/aws/iam"
 	"stargazer/video-recording/pkg/yad"
 )
 
@@ -13,7 +12,7 @@ func Initialize_backend(env_file string) {
 	config.LoadEnv(env_file)
 	yad.InitializeLogger()
 
-	utils.ExitOnError(iam.HasPermissions())
+	// utils.ExitOnError(iam.HasPermissions())
 	utils.ExitOnError(db.Pg.Connect())
 	utils.ExitOnError(db.Pg.MigrateTables())
 	utils.ExitOnError(comms.AuthenticateService())
